0b8cec51007267dd5262a3ad24b5829ac129bd99
QuocThinh9999 <111874867+QuocThinh9999@users.noreply.github.com>
2023-09-10 10:06:30 +0700

    Add project files.

---
+++ Controllers/TinhLaiXuatController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class TinhLaiXuatController : Controller
+    {
+        public IActionResult TinhLaiXuat()
+        {
+            return View();
+        }
+        [HttpPost]
+        
+    public IActionResult TinhLaiXuat(int soTien, int kyHan, double laiSuat)
+        {
+            ViewData["soTien"] = soTien;
+            ViewData["laiSuat"] = laiSuat;
+            ViewData["kyHan"] = kyHan;
+            ViewData["soTienLai"]= Math.Round(soTien * Math.Pow(1 + laiSuat / 100, kyHan)-soTien, 4);
+            ViewData["tongSoTien"]= Math.Round(soTien*Math.Pow(1+laiSuat/100,kyHan),4);
+           
+            return View();
+        }
+        
+    }
+}
